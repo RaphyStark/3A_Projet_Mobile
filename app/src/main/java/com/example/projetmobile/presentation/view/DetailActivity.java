73e3da4c406d3f34c6e998018103d0d8b5f780a7
RaphyStark <raphaelkoskas13@gmail.com>
2020-05-19 13:39:42 +0200

    Display mark name and PICTURE onClick !

--- app/src/main/java/com/example/projetmobile/presentation/view/DetailActivity.java
+++ app/src/main/java/com/example/projetmobile/presentation/view/DetailActivity.java
@@ -8,16 +8,18 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.projetmobile.Constants;
 import com.example.projetmobile.R;
 import com.example.projetmobile.Singletons;
 import com.example.projetmobile.presentation.controller.DetailController;
 import com.example.projetmobile.presentation.model.Mark;
+import com.squareup.picasso.Picasso;
 
 
 public class DetailActivity extends AppCompatActivity {
     private DetailController controller;
     private TextView txtDetail;
-    //private ImageView imageDetail;
+    private ImageView imageDetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +29,25 @@ public class DetailActivity extends AppCompatActivity {
                 this,
                 Singletons.getGson(),
                 Singletons.getSharedPreferences(getApplicationContext()));
+
         controller.onStart();
     }
 
-    public void showDetail() {
+    public void showDetail()
+    {
         txtDetail = findViewById(R.id.description_mark);
+        imageDetail = findViewById(R.id.image_mark);
         Intent intent = getIntent();
         String markJson = intent.getStringExtra("markKey");
         Mark mark = Singletons.getGson().fromJson(markJson, Mark.class);
         txtDetail.setText(mark.getName());
-        /*
+
         String pathName = Constants.BASE_URL2;
         String picName = mark.getUrl();
         String allPath = pathName+picName+".jpg";
         Picasso.get()
                 .load(allPath)
                 .into(imageDetail);
-        */
     }
 
     public void showError()
